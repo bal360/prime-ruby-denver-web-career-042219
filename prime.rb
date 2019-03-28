381e544488139936e4c1308368c0a7c123a53d72
Blake A. Long <blake.a.long@gmail.com>
2019-03-27 19:43:32 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  int_arr = (2..(int - 1)).to_a
+  if int < 2
+    return false
+  else int_arr.each do |n|
+    return false if int % n == 0
+    end
+  end
+    true
+end
